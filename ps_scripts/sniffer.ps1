2a0b3784e00ee597a90a3c1260daa0b6344ee00e
Emir Lejlic <emir@sherpaconsulting.onmicrosoft.com>
2018-11-08 10:25:47 +0100

    Using function inside the -Action scope

--- ps_scripts/sniffer.ps1
+++ ps_scripts/sniffer.ps1
@@ -2,6 +2,12 @@ $watcher = New-Object System.IO.FileSystemWatcher
 $watcher.Path  = "C:\Users\emirl\Desktop\ps_file_sniffer_test\input" # CHANGE
 $Global:logFile = ".\log.txt" # CHANGE
 
+# How to use a function inside the -Action in Register-ObjectEvent
+# Found solution here: https://stackoverflow.com/questions/42174731/powershell-console-issue-with-function-executing-in-action-of-register-objecteve
+function global:hello_world {
+    param($inputParam)
+    Write-Host "Hello World $inputParam"
+}
 
 function SetupRegisterObjectEvent {
     # Parameters
@@ -10,6 +16,7 @@ function SetupRegisterObjectEvent {
 
     # Unique ID of the listener. Avoids creating more than one
     $sourceId = "{0}_Listener" -f $EventName
+    $filename = ""
     
     Register-ObjectEvent -InputObject $args[0] -EventName $EventName -SourceIdentifier $sourceId -Action {
 
@@ -18,21 +25,18 @@ function SetupRegisterObjectEvent {
                    $Event.SourceEventArgs.FullPath, 
                    $Event.SourceEventArgs.ChangeType            
 
+        # Testing usage of function inside the -Action scope
+        global:hello_world -inputParam $Event.SourceEventArgs.ChangeType
+
         # Write string to log
         Add-Content $Global:logFile -Value $logLine
     }
+    $filename
 }
 
 # Create the listeners
 $changeTypes = "Created", "Changed", "Renamed", "Deleted"
 
 foreach ($ct in $changeTypes) {
-    if ($ct -eq "Created" -or $ct -eq "Changed") {
-        SetupRegisterObjectEvent $watcher -EventName $ct
-        # EXEC SP below        
-    } else {
-        SetupRegisterObjectEvent $watcher -EventName $ct
-    }
+    SetupRegisterObjectEvent $watcher -EventName $ct
 }
-
-#Exit
